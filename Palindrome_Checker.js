ddbeb83bd8a900527022dc461704b5ab4b5afd8f
Sebastian Muñoz <zebax97@gmail.com>
2020-05-20 21:29:36 -0500

    primer commit

---
+++ Palindrome_Checker.js
@@ -0,0 +1,7 @@
+function palindrome(str) {
+  const alphanumericOnly = str.tolowerCase().match(/[a-z0-0]/g);
+
+  return alphanumericOnly.join("") === alphanumericOnly.reverse().join("");
+}
+
+palindrome("eye");
