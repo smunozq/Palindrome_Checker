46977127082865caee0a69e291aa230c291a9d2a
Sebastian Muñoz <zebax97@gmail.com>
2020-05-20 21:32:13 -0500

    primer commit

--- Palindrome_Checker.js
+++ Palindrome_Checker.js
@@ -1,4 +1,4 @@
-function palindrome(str) {
+git pullfunction palindrome(str) {
   const alphanumericOnly = str.tolowerCase().match(/[a-z0-0]/g);
 
   return alphanumericOnly.join("") === alphanumericOnly.reverse().join("");
